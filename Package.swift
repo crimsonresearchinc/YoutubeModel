25e5e7ca46caba0a45a8badcb46d5c62929384dc
Waqar Malik <wmalloc@users.noreply.github.com>
2023-03-26 17:26:58 -0700

    Removed the linter

--- Package.swift
+++ Package.swift
@@ -12,15 +12,13 @@ let package = Package(
         .library(name: "YoutubeModel", targets: ["YoutubeModel"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin.git", .upToNextMajor(from: "0.2.2")),
-        .package(url: "https://github.com/wmalloc/WebService.git", .upToNextMajor(from: "0.6.1")),
+         .package(url: "https://github.com/wmalloc/WebService.git", .upToNextMajor(from: "0.6.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "YoutubeModel", dependencies: [],
-            plugins: [.plugin(name: "SwiftLint", package: "SwiftLintPlugin")]
+            name: "YoutubeModel", dependencies: []
         ),
         .testTarget(
             name: "YoutubeModelTests", dependencies: ["YoutubeModel",
